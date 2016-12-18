572f2f1f0dede8e9b72b599f5bfad20a0eae6b04
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-12-18 17:15:40 +0100

    Use Polygons instead of Triangles

--- gobj.go
+++ gobj.go
@@ -73,18 +73,29 @@ func (v *Vertex) Set(s []string) error {
 	return nil
 }
 
-// Triangle represents a 3-sided polygon
+// Polygon represents a polygonal face-element
 //
-// NOTE: this could easily be extended to support N-sided polygons
-// by using a []Vertex instead
-type Triangle struct {
-	P1, P2, P3 Vertex
+type Polygon []Vertex
+
+func (p *Polygon) Scale(f float64) {
+	for i := range *p {
+		(*p)[i].Scale(f)
+	}
 }
 
-func (t *Triangle) Scale(f float64) {
-	t.P1.Scale(f)
-	t.P2.Scale(f)
-	t.P3.Scale(f)
+// AABB computes and returns the axis-aligned bounding-box
+// of the polygon.
+func (p *Polygon) AABB() AABB {
+	bb := NewAABB()
+	for _, v := range *p {
+		updateMin(&bb.MinX, v.X())
+		updateMin(&bb.MinY, v.Y())
+		updateMin(&bb.MinZ, v.Z())
+		updateMax(&bb.MaxX, v.X())
+		updateMax(&bb.MaxY, v.Y())
+		updateMax(&bb.MaxZ, v.Z())
+	}
+	return bb
 }
 
 type AABB struct {
@@ -95,15 +106,15 @@ type AABB struct {
 
 // NewAABB initializes the bounding box.
 //
-// The bouding box will be valid after the first call to extend.
+// The bounding box will be valid after the first call to extend.
 func NewAABB() AABB {
 	return AABB{
-		MinX: math.MaxFloat64,
-		MinY: math.MaxFloat64,
-		MinZ: math.MaxFloat64,
-		MaxX: -math.MaxFloat64,
-		MaxY: -math.MaxFloat64,
-		MaxZ: -math.MaxFloat64,
+		MinX: math.Inf(1),
+		MinY: math.Inf(1),
+		MinZ: math.Inf(1),
+		MaxX: math.Inf(-1),
+		MaxY: math.Inf(-1),
+		MaxZ: math.Inf(-1),
 	}
 }
 
@@ -133,45 +144,18 @@ func (bb AABB) String() string {
 		bb.MinZ, bb.MaxZ)
 }
 
-// AABB computes and returns the axis-aligned bounding-box
-// of the triangle.
-func (t Triangle) AABB() AABB {
-	return AABB{
-		MinX: math.Min(t.P1.X(), math.Min(t.P2.X(), t.P3.X())),
-		MinY: math.Min(t.P1.Y(), math.Min(t.P2.Y(), t.P3.Y())),
-		MinZ: math.Min(t.P1.Z(), math.Min(t.P2.Z(), t.P3.Z())),
-		MaxX: math.Max(t.P1.X(), math.Max(t.P2.X(), t.P3.X())),
-		MaxY: math.Max(t.P1.Y(), math.Max(t.P2.Y(), t.P3.Y())),
-		MaxZ: math.Max(t.P1.Z(), math.Max(t.P2.Z(), t.P3.Z())),
-	}
-}
-
-func (t Triangle) isDegenerate() bool {
-	// find the determinant of the 3x3 matrix in which the triangle coords can
-	// be represented, it's 0 or close to 0.0, the triangle area is null and we
-	// consider the triangle as degenerate
-	det := (t.P1.X() * t.P2.Y() * t.P3.Z()) +
-		(t.P1.Y() * t.P2.Z() * t.P3.X()) +
-		(t.P1.Z() * t.P2.X() * t.P3.Y()) -
-		(t.P1.Z() * t.P2.Y() * t.P3.X()) -
-		(t.P1.Y() * t.P2.X() * t.P3.Z()) -
-		(t.P1.X() * t.P2.Z() * t.P3.Y())
-
-	return math.Abs(det) < 1e-5
-}
-
 type OBJFile struct {
-	vertices  []Vertex
-	triangles []Triangle
-	aabb      AABB
+	vertices []Vertex
+	polygons []Polygon
+	aabb     AABB
 }
 
 func (of OBJFile) Vertices() []Vertex {
 	return of.vertices
 }
 
-func (of OBJFile) Triangles() []Triangle {
-	return of.triangles
+func (of OBJFile) Polygons() []Polygon {
+	return of.polygons
 }
 
 func (of OBJFile) AABB() AABB {
@@ -190,43 +174,27 @@ func (of *OBJFile) parseVertex(kw string, data []string) error {
 }
 
 func (of *OBJFile) parseFace(kw string, data []string) error {
-	if len(data) != 3 {
-		return errors.New("only triangular faces are supported")
-	}
-
-	var (
-		vi  [3]int
-		err error
-	)
-
-	// vertices indices
-	for i, s := range data {
-		// we are only interested in the vertex index
+	var p Polygon // polygonal face currently filled
+	for _, s := range data {
+		// read the indices of the face vertices
 		sidx := strings.Split(s, "/")[0]
-		vi[i], err = strconv.Atoi(sidx)
+		vidx, err := strconv.Atoi(sidx)
 		if err != nil {
 			return fmt.Errorf("invalid vertex coordinate value \"%s\"", s)
 		}
+		p = append(p, of.vertices[vidx-1])
 	}
 
-	t := Triangle{
-		P1: of.vertices[vi[0]-1],
-		P2: of.vertices[vi[1]-1],
-		P3: of.vertices[vi[2]-1],
-	}
-
-	// extend the mesh bounding box with the triangle
-	of.aabb.extend(t.AABB())
-
-	of.triangles = append(of.triangles, t)
+	// extend the mesh bounding box with the polygon's one
+	of.aabb.extend(p.AABB())
+	of.polygons = append(of.polygons, p)
 	return nil
 }
 
 func (of *OBJFile) DumpInfo() string {
-	var nfo string
-	nfo = fmt.Sprintln("num vertices :", len(of.vertices))
-	nfo += fmt.Sprintln("num triangles:", len(of.triangles))
-	nfo += fmt.Sprintln("bounding box :", of.aabb)
+	nfo := fmt.Sprintln("num verts:", len(of.vertices))
+	nfo += fmt.Sprintln("num polys:", len(of.polygons))
+	nfo += fmt.Sprintln("aabb     :", of.aabb)
 	return nfo
 }
 
