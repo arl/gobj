b91416d2489209b1282b0c7f2305d855c8a09070
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-12-19 11:06:52 +0100

    Split Polygon definition in its own file

--- gobj.go
+++ gobj.go
@@ -2,83 +2,9 @@ package gobj
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 	"strings"
 )
-
-// Polygon represents a polygonal face-element.
-type Polygon []Vertex
-
-// Scale applies f scale factor to every coord of the polygon vertices.
-func (p *Polygon) Scale(f float64) {
-	for i := range *p {
-		(*p)[i].Scale(f)
-	}
-}
-
-// AABB computes and returns the axis-aligned bounding-box
-// of the polygon.
-func (p *Polygon) AABB() AABB {
-	bb := NewAABB()
-	for _, v := range *p {
-		updateMin(&bb.MinX, v.X())
-		updateMin(&bb.MinY, v.Y())
-		updateMin(&bb.MinZ, v.Z())
-		updateMax(&bb.MaxX, v.X())
-		updateMax(&bb.MaxY, v.Y())
-		updateMax(&bb.MaxZ, v.Z())
-	}
-	return bb
-}
-
-// AABB is an axis-aligned bounding box.
-type AABB struct {
-	MinX, MaxX float64
-	MinY, MaxY float64
-	MinZ, MaxZ float64
-}
-
-// NewAABB initializes the bounding box.
-//
-// The bounding box will be valid after the first call to extend.
-func NewAABB() AABB {
-	return AABB{
-		MinX: math.Inf(1),
-		MinY: math.Inf(1),
-		MinZ: math.Inf(1),
-		MaxX: math.Inf(-1),
-		MaxY: math.Inf(-1),
-		MaxZ: math.Inf(-1),
-	}
-}
-
-func (bb *AABB) extend(other AABB) {
-	// update the min and max for each coord
-	updateMin(&bb.MinX, other.MinX)
-	updateMin(&bb.MinY, other.MinY)
-	updateMin(&bb.MinZ, other.MinZ)
-	updateMax(&bb.MaxX, other.MaxX)
-	updateMax(&bb.MaxY, other.MaxY)
-	updateMax(&bb.MaxZ, other.MaxZ)
-}
-
-// Scale scales the axis aligned bounding box.
-func (bb *AABB) Scale(f float64) {
-	bb.MinX *= f
-	bb.MinY *= f
-	bb.MinZ *= f
-	bb.MaxX *= f
-	bb.MaxY *= f
-	bb.MaxZ *= f
-}
-
-func (bb AABB) String() string {
-	return fmt.Sprintf("x[%f, %f], y[%f, %f], z[%f, %f]",
-		bb.MinX, bb.MaxX,
-		bb.MinY, bb.MaxY,
-		bb.MinZ, bb.MaxZ)
-}
 
 // OBJFile describes the content of an OBJ geometry definition file.
 type OBJFile struct {
